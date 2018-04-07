03544da7db489046575f5969eae38f6f3d9a4009
tonyfast <tony.fast@gmail.com>
2018-04-07 12:02:26 -0400

    Firest commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+__import__('setuptools').setup(
+    name="sweet",
+    version="0.0.1",
+    author="deathbeds", author_email="tony.fast@gmail.com",
+    description="Interactive notebook testing.", 
+    license="BSD-3-Clause",
+    install_requires=[],
+    include_package_data=True,
+    packages=__import__('setuptools').find_packages(),
+)
---
+++ sweet.py
@@ -0,0 +1,132 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+from hypothesis import assume, HealthCheck, Verbosity, settings
+
+from unittest import TestLoader, TestResult, TestSuite, TestCase
+from dataclasses import dataclass, field
+from functools import partial
+doctest = True
+unittest = True
+loader = TestLoader()
+if __name__ == '__main__':
+    get_ipython().run_line_magic('load_ext', 'rites')
+    get_ipython().run_line_magic('rites', 'autoreload')
+    get_ipython().run_line_magic('autoreload', '2')
+
+
+# In[2]:
+
+
+def discover(module='__main__', suite=None, *, doctest=doctest, unittest=unittest, loader=loader):
+    from doctest import DocTestSuite
+    if isinstance(module, str): module = __import__('importlib').import_module(module)
+    if suite is None:
+        return Sweet(module=module, doctest=doctest, unittest=unittest, loader=loader)    
+    
+    for name, object in vars(module).items():
+        if unittest:
+            if isinstance(object, type) and issubclass(object, TestCase):
+                suite.addTests(loader.loadTestsFromTestCase(object)._tests)
+            
+            if callable(object) and not isinstance(object, (partial, type)):
+                function_test_case = infer(object)
+                if function_test_case:
+                    suite.addTest(function_test_case)
+                
+    if doctest:
+        suite.addTests(DocTestSuite(module)._tests)
+    return suite
+
+
+# In[4]:
+
+
+def infer(object)->TestCase:
+    """Use the hypothesis inference systems to create automated types from the annotations or ghetto typing.
+    
+    
+    >>> def f(int, b:int): ...
+    >>> test = infer(f)
+    """
+    from unittest import FunctionTestCase
+    from inspect import getfullargspec
+    from hypothesis import given, strategies as st, find
+    spec = getfullargspec(object)
+    annotations = dict(**spec.annotations)
+    returns = annotations.pop('return', None)
+    
+    if not spec.args: return FunctionTestCase(object)
+    
+            
+    if not (spec.defaults or spec.kwonlydefaults):
+        annotations = {
+            str: st.from_type(object) if isinstance(object, type) and getattr(object, '__name__', '') != 'object'
+            else object if isinstance(object, st.SearchStrategy)
+            else st.one_of(list(map(st.just, object)))
+            for str, object in annotations.items()
+        }
+        if annotations:
+            return FunctionTestCase(given(**annotations)(object))
+            
+            
+    if (returns is not None) and callable(returns) ^ isinstance(returns, type) and len(annotations) is 1:
+        return FunctionTestCase(partial(find, *annotations.values(), lambda x: returns(object(x))))
+
+
+# In[18]:
+
+
+@dataclass
+class Sweet(TestSuite):
+    """A TestSuite.
+    """
+    _tests: list = field(default_factory=list)
+    module: __import__('types').ModuleType = field(default=__import__('__main__'))
+    loader: TestLoader = field(default_factory=TestLoader, repr=False)
+    result: TestResult = field(default_factory=TestResult)
+    doctest: bool = True
+    unittest: bool = True
+    
+    def __post_init__(Discover): 
+        global doctest, unittest
+        from importlib import import_module
+        TestSuite.__init__(Discover)
+        if isinstance(Discover.module, str): 
+            Discover.module = import_module(Discover.module)
+        if not Discover._tests:
+            Discover.discover()  
+        
+    def discover(Discover): 
+        return discover(module=Discover.module, suite=Discover, doctest=Discover.doctest, unittest=Discover.unittest)
+    
+    def run(Discover, result=None, debug=False):
+        try: return super().run(result or Discover.result, debug=debug)
+        except type('Quack', (BaseException,), {}): ...
+        finally: Discover._tests = list(filter(bool, Discover._tests))
+
+
+# In[19]:
+
+
+settings.register_profile('ip', settings(
+        suppress_health_check=(HealthCheck.return_value,),
+        verbosity=Verbosity.normal,))
+
+def load_ipython_extension(ip=None):
+    settings.load_profile('ip')
+
+
+# In[ ]:
+
+
+if __name__ == '__main__':
+    get_ipython().system('jupyter nbconvert --to python sweet.ipynb')
+    from importlib import reload
+    result = Sweet(module='readme').run()
+    print(result)
+    print(Sweet(module=__name__).run(result))
+
